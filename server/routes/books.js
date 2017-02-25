c7f24bad9d76f7f82c3562a3002b39cff81c6443
RyanYizhaoHuang <ryan.yizhao@gmail.com>
2017-02-25 09:42:55 -0500

    Add edit function

--- server/routes/books.js
+++ server/routes/books.js
@@ -44,6 +44,7 @@ router.post('/add', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
+    //create a newBook object, and get the req from the form
     let newBook = book({
       "Title": req.body.title,
       "Price": req.body.price,
@@ -62,20 +63,67 @@ router.post('/add', (req, res, next) => {
 
 });
 
+// Edit book detail- Get info
 // GET the Book Details page in order to edit an existing Book
 router.get('/:id', (req, res, next) => {
 
     /*****************
      * ADD CODE HERE *
      *****************/
+     //get id from body parameter 
+     try {
+      // get a reference to the id from the url
+      let id = mongoose.Types.ObjectId.createFromHexString(req.params.id);
+
+      // find one book by its id
+      book.findById(id, (err, books) => {
+        if(err) {
+          console.log(err);
+          res.end(error);
+        } else {
+          // show the book details view
+          res.render('books/details', {
+              title: 'Edit Book Details',
+              books: books,
+              displayName: req.user ? req.user.displayName : ''
+          });
+        }
+      });
+    } catch (err) {
+      console.log(err);
+      res.redirect('/errors/404');
+    }
+
 });
 
+// Edit book detail - Update info
 // POST - process the information passed from the details form and update the document
 router.post('/:id', (req, res, next) => {
 
     /*****************
      * ADD CODE HERE *
      *****************/
+     // get id from the URL then pass the req body to update Book object
+    let id = req.params.id;
+
+     let updatedBook = book({
+       "_id": id,
+      "Title": req.body.title,
+      "Price": req.body.price,
+      "Author": req.body.author,
+      "Genre": req.body.genre
+    });
+
+    book.update({_id: id}, updatedBook, (err) => {
+      if(err) {
+        console.log(err);
+        res.end(err);
+      } else {
+        // refresh the book List
+        res.redirect('/books');
+      }
+    });
+
 
 });
 
